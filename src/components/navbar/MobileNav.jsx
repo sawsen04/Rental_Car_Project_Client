41005733d603c7d5ec425b099b00d83f21c2ca4d
sawsen04 <sawsenayari87@gmail.com>
2024-03-15 01:50:47 +0100

    updte

--- src/components/navbar/MobileNav.jsx
+++ src/components/navbar/MobileNav.jsx
@@ -1,6 +1,6 @@
 function MobileNav({ children }) {
   return (
-    <div className="md:hidden  fixed top-[55px] right-[22px] border-2 border-red-600 bg-white w-[50%] flex flex-col justify-center gap-3">
+    <div className="md:hidden  fixed top-[55px] right-[22px] bg-white w-[50%] flex flex-col justify-center gap-3">
       {children}
     </div>
   );
